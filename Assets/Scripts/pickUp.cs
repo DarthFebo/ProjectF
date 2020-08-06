0efe901f0a49580db4535d2ab846e84cbf81e903
Fvanbrummelen <darthfebo@users.noreply.github.com>
2020-08-06 22:57:30 +0200

    geprobeerd movement te herschrijven met code van vorige game. ongewilde bestandentroep meegekomen. De spinnen hebben een animator met een animatorsystem en de deuren kunnnen nu open met een trigger wanneer je dichterbij komt. De radio heeft een audioscript die reageert op E en een range waarbinnen die te horen is,

---
+++ Assets/Scripts/pickUp.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class pickUp : MonoBehaviour{
+   
+    
+    public Transform theDest;
+    void OnMouseDown()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        //  GetComponent<Rigidbody>().freezeRotation = true;
+        this.transform.position = theDest.position;
+        this.transform.parent = GameObject.Find("destination").transform;
+    }
+    void OnMouseUp()
+    {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = false;
+    }
+    
+}
+
